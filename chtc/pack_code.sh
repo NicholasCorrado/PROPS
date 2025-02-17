7c809d97f72440c309c89c6c8341fc20d1b876c3
Nicholas Corrado <ncorrado@wisc.edu>
2025-02-17 13:40:26 -0600

    update chtc scripts

--- chtc/pack_code.sh
+++ chtc/pack_code.sh
@@ -1,4 +1,4 @@
 cd ../.. # cd just outside the repo
-tar --exclude="chtc" --exclude='src/results' --exclude='results_chtc' -czvf PROPS.tar.gz PROPS
+tar --exclude="chtc" --exclude='local' --exclude='results' --exclude='.git'  -czvf PROPS.tar.gz PROPS
 scp PROPS.tar.gz ncorrado@ap2001.chtc.wisc.edu:/staging/ncorrado
 rm PROPS.tar.gz
