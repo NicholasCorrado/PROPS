4b82a1a7e14670ba21e256f012d36f8f6aeb02ae
Nicholas Corrado <ncorrado@wisc.edu>
2025-02-22 13:15:41 -0600

    patch for mujoco jobs

--- chtc/job.sh
+++ chtc/job.sh
@@ -4,7 +4,7 @@ pid=$1  # ranges from 0 to num_commands*num_jobs-1
 step=$2 # ranges from 0 to num_jobs-1
 cmd=`tr '*' ' ' <<< $3` # replace * with space
 cmd="${cmd} --run_id ${step} --seed ${step}"
-echo $cmd $pid $step
+echo $cmd
 
 # fetch your code from /staging/
 CODENAME=PROPS
@@ -12,17 +12,11 @@ cp /staging/ncorrado/${CODENAME}.tar.gz .
 tar -xzf ${CODENAME}.tar.gz
 rm ${CODENAME}.tar.gz
 
-# Dcoker work-around
-#python3 -m venv env
-#source env/bin/activate
-#cd PROPS
-#pip install -r requirements.txt
-
 cd PROPS
 export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
-#pip install --user -e custom-envs
+mkdir gymnasium_local
+python -m pip install 'gymnasium[mujoco]==0.29.1' --target gymnasium_local
 
-# run your script -- $step ensures seeding is consistent across experiment batches
 $cmd
 
 # compress results. This file will be transferred to your submit node upon job completion.
